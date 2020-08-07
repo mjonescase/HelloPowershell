aad8142e1de408a37184654b986c6186b7136281
Mike Jones <mjones@medasync.com>
2020-08-06 20:26:17 -0700

    make progress on the directory script

---
+++ HelloPowershell/HelloPowershell.psm1
@@ -0,0 +1,25 @@
+# Get public and private function definition files.
+[System.Object[]] $Public = Get-ChildItem $(Join-Path HelloPowershell Public) -Filter *.ps1
+[System.Object[]] $Private = Get-ChildItem $(Join-Path HelloPowershell Private) -Filter *.ps1
+
+# Dot source the files
+Foreach($import in @($Public + $Private))
+{
+    try {
+        . $import.fullname
+    }
+    catch {
+        Write-Error "Failed to import the function $($import.fullname): $_"
+    }
+}
+
+# Load up dependency modules
+foreach($Module in $PrivateModules)
+{
+    try {
+        Import-Module $Module -ErrorAction Stop
+    }
+    catch {
+        Write-Error "Failed to import module $Module`: $_"
+    }
+}
