e19adc838f30e3590a86b55278b3ad4a84010886
Mike Jones <mjones@medasync.com>
2020-08-28 21:23:17 -0700

    make the contents of the two test files exactly the same

--- Tests/Assert-PayingInFullMakesSense.Tests.ps1
+++ Tests/Assert-PayingInFullMakesSense.Tests.ps1
@@ -1,4 +1,4 @@
-if(-not $ENV:BHProjectPath)
+<# if(-not $ENV:BHProjectPath)
 {
     Set-BuildEnvironment -Path $PSScriptRoot\.. -Force
 }
@@ -21,6 +21,32 @@ InModuleScope 'HelloPowershell' {
             It 'Returns False' {
                 # $Result = Get-Greeting # Assert-PayingInFullMakesSense -Discount 0.1
                 # $Result.DoesPayInFullMakeSense | Should Be $false
+                Get-Greeting | Should Be "Hello World!"
+            }
+        }
+    }
+} #>if(-not $ENV:BHProjectPath)
+{
+    Set-BuildEnvironment -Path $PSScriptRoot\.. -Force
+}
+
+Remove-Module $ENV:BHProjectName -ErrorAction SilentlyContinue
+Import-Module (Join-Path $ENV:BHProjectPath $ENV:BHProjectName) -Force
+
+
+InModuleScope 'HelloPowershell' {
+    $PSVersion = $PSVersionTable.PSVersion.Major
+
+    $Verbose = @{}
+    if($ENV:BHBranchName -notlike "mainline" -or $env:BHCommitMessage -match "!verbose")
+    {
+        $Verbose.Add("Verbose",$True)
+    }
+
+    Describe "Get-Greeting PS$PSVersion" {
+        Context 'Gets Greeting' {
+
+            It 'Gets the greeting' {
                 Get-Greeting | Should Be "Hello World!"
             }
         }
